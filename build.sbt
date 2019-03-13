2b3f8f056262ca51b1ca9b692c46004e78b43bef
roy <newroyker@gmail.com>
2019-03-13 15:28:16 -0700

    add foo counter

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "1.0"
 scalaVersion := "2.11.12"
 
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.0"
+
+libraryDependencies +="org.eclipse.jetty" % "jetty-servlet" % "9.3.20.v20170531"
--- src/main/scala/com/roy/SparkApp.scala
+++ src/main/scala/com/roy/SparkApp.scala
@@ -1,5 +1,7 @@
 package com.roy
 
+import org.apache.spark.SparkEnv
+import org.apache.spark.metrics.source.MySource
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.ScalaReflection
 import org.apache.spark.sql.execution.QueryExecution
@@ -19,12 +21,15 @@ object SparkApp extends App {
     .config("spark.driver.host", "localhost")
     .config("spark.sql.streaming.metricsEnabled", "true") //OPTION 3
     .config("spark.metrics.conf.*.sink.console.class", "org.apache.spark.metrics.sink.ConsoleSink")
+    .config("spark.metrics.conf.*.source.mysource.class", "org.apache.spark.metrics.source.MySource")
     .getOrCreate()
 
   spark.sparkContext.setLogLevel("ERROR")
 
+  import spark.implicits._
+
   //OPTION 1
-  spark.streams.addListener(new StreamingQueryListener{
+  spark.streams.addListener(new StreamingQueryListener {
     override def onQueryStarted(event: StreamingQueryListener.QueryStartedEvent): Unit = {
       println(s">>> Start: ${event.id}")
     }
@@ -49,13 +54,19 @@ object SparkApp extends App {
     }
   })
 
-  val schema = ScalaReflection.schemaFor[Emp].dataType.asInstanceOf[StructType]
+  val schema: StructType = ScalaReflection.schemaFor[Emp].dataType.asInstanceOf[StructType]
 
   val query = spark
     .readStream
     .option("maxFilesPerTrigger", 1) // to trickle in the data
     .schema(schema)
     .json("src/main/resources/input")
+    .as[Emp]
+    .map(e => {
+      val source: MySource = SparkEnv.get.metricsSystem.getSourcesByName("MySource").head.asInstanceOf[MySource]
+      source.FOO_COUNTER.inc(1L)
+      e
+    })
     .groupBy(col("dept"))
     .count()
     .writeStream
